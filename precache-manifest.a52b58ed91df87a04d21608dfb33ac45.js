ed1d18df143d5cb3a0b793d53770d051d9f96bda
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-03 03:33:01 -0800

    Updates

---
+++ precache-manifest.a52b58ed91df87a04d21608dfb33ac45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f48432bbeaa2e512bdd9a52635ec8a53",
+    "url": "/redux-anecdotes/index.html"
+  },
+  {
+    "revision": "7bc14b5f8ac9210cb4e0",
+    "url": "/redux-anecdotes/static/js/2.44664352.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/redux-anecdotes/static/js/2.44664352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77dba961d99fd55ab242",
+    "url": "/redux-anecdotes/static/js/main.0737cfcc.chunk.js"
+  },
+  {
+    "revision": "8cdd5c6a0b17556c1087",
+    "url": "/redux-anecdotes/static/js/runtime-main.1195a160.js"
+  }
+]);
